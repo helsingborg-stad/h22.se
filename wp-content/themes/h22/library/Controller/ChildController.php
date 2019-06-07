219eb428b9270a8b8acbfbf8070a164cc6e53c08
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-06-07 14:20:44 +0200

    Add main menu (top nav)

--- wp-content/themes/h22/library/Controller/ChildController.php
+++ wp-content/themes/h22/library/Controller/ChildController.php
@@ -11,6 +11,24 @@ class ChildController
 
     public function data($data)
     {
+        $data['mainMenu'] = $this->getWordpressMenuItemsBySlug('main-menu');
+
         return $data;
+    }
+
+    /**
+     * Returns array of Wordpress menu items
+     * @param string Slug of a registred menu
+     * @return array
+     */
+    public function getWordpressMenuItemsBySlug($slug)
+    {
+        if (empty(get_nav_menu_locations()) || !isset(get_nav_menu_locations()[$slug])) {
+            return array();
+        }
+
+        $menu = new \Municipio\Helper\Menu(get_nav_menu_locations()[$slug]);
+
+        return $menu->wpMenu;
     }
 }
