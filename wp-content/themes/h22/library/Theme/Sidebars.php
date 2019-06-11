c8d7143085615602f9c0321efd246a9f8ff27428
Younus <younus.abdalla@whitespace.se>
2019-06-11 17:34:13 +0200

    ✨ Register sidebar for logotypes in footer

---
+++ wp-content/themes/h22/library/Theme/Sidebars.php
@@ -0,0 +1,23 @@
+<?php
+namespace H22\Theme;
+
+class Sidebars
+{
+    public function __construct()
+    {
+        add_action('widgets_init', array($this, 'register'));
+    }
+
+    public function register()
+    {
+        register_sidebar(array(
+            'id'            => 'footer-area-logos',
+            'name'          => __('Sidfot logotyper', 'municipio'),
+            'description'   => __('Visa upp logotyper i fullbredd nedanför den vanliga sidfoten', 'municipio'),
+            'before_widget' => '<div class="c-footer__logo"><div class="%2$s">',
+            'after_widget'  => '</div></div>',
+            'before_title'  => '<h2 class="footer-title">',
+            'after_title'   => '</h2>'
+        ));
+    }
+}
