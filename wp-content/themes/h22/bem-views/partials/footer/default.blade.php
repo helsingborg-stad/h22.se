caff8a2f49930e95478dc762d1b3985e661af533
Younus <younus.abdalla@whitespace.se>
2019-06-11 15:21:25 +0200

    ✨ Enable widgets in footer area

--- wp-content/themes/h22/bem-views/partials/footer/default.blade.php
+++ wp-content/themes/h22/bem-views/partials/footer/default.blade.php
@@ -7,12 +7,14 @@
         <?php dynamic_sidebar('bottom-sidebar'); ?>
       </div>
     @endif
-@stop 
+@stop
 
 @section('footer-body')
-    <div class="c-footer c-footer--h22">
+    <div class="c-footer">
         <div class="c-footer__body">
-            
+            @if (is_active_sidebar('footer-area'))
+                <?php dynamic_sidebar('footer-area'); ?>
+            @endif
         </div>
     </div>
 @stop
