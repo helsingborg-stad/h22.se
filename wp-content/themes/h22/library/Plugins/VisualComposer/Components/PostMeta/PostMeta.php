7ae6f33ceed21d3a19a4d0e57a040596c32507e8
Fredrik Johansson <jeanfredrik@gmail.com>
2019-06-12 15:00:46 +0200

    👌 Don’t show settings on create for `vc_h22_post_meta`

--- wp-content/themes/h22/library/Plugins/VisualComposer/Components/PostMeta/PostMeta.php
+++ wp-content/themes/h22/library/Plugins/VisualComposer/Components/PostMeta/PostMeta.php
@@ -11,6 +11,7 @@ if (!class_exists('\H22\Plugins\VisualComposer\Components\PostMeta\PostMeta')):
                 'name' => __('Post Meta', 'h22'),
                 'base' => 'vc_h22_post_meta',
                 'icon' => 'vc_icon-vc-gitem-post-excerpt',
+                'show_settings_on_create' => false,
                 'params' => array(),
                 'html_template' => dirname(__FILE__) . '/PostMeta.php',
             );
@@ -22,10 +23,8 @@ if (!class_exists('\H22\Plugins\VisualComposer\Components\PostMeta\PostMeta')):
 
         public function prepareData($data)
         {
-
-          $data['attributes']['class'][] = 'c-post-meta';
-          $data['post_meta'] = get_the_date();
-
+            $data['attributes']['class'][] = 'c-post-meta';
+            $data['post_meta'] = get_the_date();
 
             return $data;
         }
