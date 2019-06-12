bb80dd3bb71324fd35735effc34d728340330b67
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-06-12 14:05:39 +0200

    wrap with element class

--- wp-content/themes/h22/library/Plugins/VisualComposer/Components/Teaser/views/teaser.blade.php
+++ wp-content/themes/h22/library/Plugins/VisualComposer/Components/Teaser/views/teaser.blade.php
@@ -1,11 +1,13 @@
-<div{!! html_build_attributes($attributes) !!}>
-    <div class="c-teaser__meta">{{ $meta_heading }}</div>
-    <h2>{{ $heading }}</h2>
-    <p class="preamble">{{ $preamble }}</p>
-    {!! $content !!}
-    @if ($link)
-        <a{!! html_build_attributes($link['attributes']) !!}>
-            {{ $link['text'] }}
-        </a>
-    @endif
+<div class="c-element">
+    <div{!! html_build_attributes($attributes) !!}>
+        <div class="c-teaser__meta">{{ $meta_heading }}</div>
+        <h2>{{ $heading }}</h2>
+        <p class="preamble">{{ $preamble }}</p>
+        {!! $content !!}
+        @if ($link)
+            <a{!! html_build_attributes($link['attributes']) !!}>
+                {{ $link['text'] }}
+            </a>
+        @endif
+    </div>
 </div>
