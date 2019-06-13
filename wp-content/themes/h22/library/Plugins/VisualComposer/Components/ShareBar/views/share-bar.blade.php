3bb065afb2f0de7bd18ce9d068668cd6db44aa33
Fredrik Johansson <jeanfredrik@gmail.com>
2019-06-13 15:25:10 +0200

    👌 Update `vc_h22_share_bar` style

--- wp-content/themes/h22/library/Plugins/VisualComposer/Components/ShareBar/views/share-bar.blade.php
+++ wp-content/themes/h22/library/Plugins/VisualComposer/Components/ShareBar/views/share-bar.blade.php
@@ -1,6 +1,4 @@
-<div class="c-share-bar grid u-p-4">
-    <div class="grid-md-4 offset-md-8 u-mb-0">
-        <h4 class="c-share-bar__heading">{{ __('Share this page') }}</h4>
-        @include('partials.social-share')
-    </div>
+<div class="c-share-bar">
+    <h4 class="c-share-bar__heading">{{ __('Share this page') }}</h4>
+    @include('partials.social-share')
 </div>
