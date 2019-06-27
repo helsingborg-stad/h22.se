446a592fdcf05fd769f9ec8e530043ce11ca2e21
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-06-27 11:34:31 +0200

    Only output if image is avalible

--- wp-content/themes/h22/library/Plugins/VisualComposer/Components/VcSingleImage/views/vc-single-image.blade.php
+++ wp-content/themes/h22/library/Plugins/VisualComposer/Components/VcSingleImage/views/vc-single-image.blade.php
@@ -1,10 +1,12 @@
-<div class="c-single-image c-single-image--{!! $single_image['size'] !!} {!! $el_class !!}">
-    <div class="c-single-image__inner">
-        <div class="c-single-image__image-wrapper">
-            <img {!! html_build_attributes($single_image['attributes']) !!}>
+@if (isset($data['image']) && !empty($data['image']))
+    <div class="c-single-image c-single-image--{!! $single_image['size'] !!} {!! $el_class !!}">
+        <div class="c-single-image__inner">
+            <div class="c-single-image__image-wrapper">
+                <img {!! html_build_attributes($single_image['attributes']) !!}>
+            </div>
+            @if($single_image['caption'])
+            <p class="c-single-image__image-caption">{{$single_image['caption']}}</p>
+            @endif
         </div>
-        @if($single_image['caption'])
-        <p class="c-single-image__image-caption">{{$single_image['caption']}}</p>
-        @endif
     </div>
-</div>
+@endif
