03a4d14b6ae0fc9bf0328d0ff2d2505875d3a6b9
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-07-03 13:22:53 +0200

    Add wrapper

--- wp-content/themes/h22/library/Plugins/VisualComposer/Components/ShareBar/views/share-bar.blade.php
+++ wp-content/themes/h22/library/Plugins/VisualComposer/Components/ShareBar/views/share-bar.blade.php
@@ -1,4 +1,6 @@
-<div class="c-share-bar">
-    <span class="c-share-bar__heading o-text-meta u-display-block">{{ __('Share this page') }}</span>
-    @include('partials.social-share')
+<div class="c-element">
+    <div class="c-share-bar">
+        <span class="c-share-bar__heading o-text-meta u-display-block">{{ __('Share this page') }}</span>
+        @include('partials.social-share')
+    </div>
 </div>
