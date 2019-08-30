2b32d701edff9f5b7f0838062bcabb89ec725706
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-08-30 17:00:07 +0200

    smooth anchor links (v e r y smoooooth)

--- wp-content/themes/h22/assets/source/js/app.js
+++ wp-content/themes/h22/assets/source/js/app.js
@@ -7,3 +7,13 @@ import './vc-tta-section';
 
 objectFitVideos();
 objectFitImages();
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
