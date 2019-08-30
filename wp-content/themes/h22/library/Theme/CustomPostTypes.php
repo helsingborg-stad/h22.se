a171be10281e346ad2f9d3c78d190118a0f2e6f2
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-08-30 11:07:59 +0200

    Register speaker posttype

--- wp-content/themes/h22/library/Theme/CustomPostTypes.php
+++ wp-content/themes/h22/library/Theme/CustomPostTypes.php
@@ -67,5 +67,36 @@ class CustomPostTypes
                 'menu_position' => 10,
             )
         );
+
+        new \H22\Entity\PostType(
+            _x('Speakers', 'Post type plural', 'h22'),
+            _x('Speaker', 'Post type singular', 'h22'),
+            'speaker',
+            array(
+                'description' => __('H22 Projects', 'h22'),
+                'menu_icon' => 'dashicons-portfolio',
+                'public' => true,
+                'publicly_queriable' => true,
+                'show_ui' => true,
+                'show_in_nav_menus' => true,
+                'has_archive' => true,
+                // 'rewrite'              =>   array(
+                //     'slug'       =>   __('projects', 'h22'),
+                //     'with_front' =>   false
+                // ),
+                'hierarchical' => false,
+                'exclude_from_search' => false,
+                'taxonomies' => array('category', 'post_tag'),
+                'supports' => array(
+                    'title',
+                    'editor',
+                    'author',
+                    'thumbnail',
+                    'excerpt',
+                ),
+                'show_in_rest' => true,
+                'menu_position' => 10,
+            )
+        );
     }
 }
