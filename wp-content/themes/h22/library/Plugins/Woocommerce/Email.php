701d52a98276c009df45df7c60af4b9d0148ac3d
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-09-06 18:46:11 +0200

    Include order summary to all attendees

---
+++ wp-content/themes/h22/library/Plugins/Woocommerce/Email.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace H22\Plugins\Woocommerce;
+
+class Email
+{
+    public function __construct()
+    {
+
+        add_filter('theme/h22/woocommerce/email/customer-complete-order/firstName', array($this, 'replaceFirstNameForAttendees'), 10, 3);
+        add_filter('theme/h22/woocommerce/email/customer-complete-order/additionalContent', array($this, 'replaceEmailContentForAttendees'), 10, 3);
+        add_action('woocommerce_order_status_completed', array($this, 'sendCustomAttendeeMails'), 10, 1);
+    }
+
+    public function sendCustomAttendeeMails($orderId)
+    {
+        $attendees = array_filter(FooEvents::getAttendees($orderId), function ($attendee) {
+            return isset($attendee['WooCommerceEventsPurchaserEmail'])
+                && isset($attendee['WooCommerceEventsAttendeeEmail'])
+                && $attendee['WooCommerceEventsPurchaserEmail'] !== $attendee['WooCommerceEventsAttendeeEmail'];
+        });
+
+        if (empty($attendees) || !is_a($order = wc_get_order($orderId), 'WC_Order')) {
+            return;
+        }
+
+        foreach ($attendees as $attendee) {
+            $orderCompleteMail = \WC_Emails::instance()->emails['WC_Email_Customer_Completed_Order'];
+
+            $orderCompleteMail->setup_locale();
+            $orderCompleteMail->object = $order;
+            $orderCompleteMail->recipient = $attendee['WooCommerceEventsAttendeeEmail'];
+            // $orderCompleteMail->placeholders['{order_date}'] = wc_format_datetime($orderCompleteMail->object->get_date_created());
+            // $orderCompleteMail->placeholders['{order_number}'] = $orderCompleteMail->object->get_order_number();
+
+            if ($orderCompleteMail->is_enabled() && $orderCompleteMail->get_recipient()) {
+                $orderCompleteMail->send($orderCompleteMail->get_recipient(), $orderCompleteMail->get_subject(), $orderCompleteMail->get_content(), $orderCompleteMail->get_headers(), $orderCompleteMail->get_attachments());
+            }
+            $orderCompleteMail->restore_locale();
+        }
+    }
+
+    public function replaceFirstNameForAttendees($firstName, $order, $email)
+    {
+        $orderId = $order->get_data()['id'];
+        $recipient = $email->recipient;
+
+        if ($attendee = FooEvents::getAttendeeByMail($orderId, $recipient)) {
+            $firstName = $attendee['WooCommerceEventsAttendeeName'];
+        }
+
+        return $firstName;
+    }
+
+    public function replaceEmailContentForAttendees($content, $order, $email)
+    {
+        $orderId = $order->get_data()['id'];
+        $recipient = $email->recipient;
+        $content = !empty(self::getAttendeeEmailContent($orderId, $recipient)) ? self::getAttendeeEmailContent($orderId, $recipient) : $content;
+
+        return $content;
+    }
+
+    public static function getAttendeeEmailContent($orderId, $attendeeMail)
+    {
+        if (!$attendee = FooEvents::getAttendeeByMail($orderId, $attendeeMail)) {
+            return '';
+        }
+
+        $customAttendeeFields = $attendee['WooCommerceEventsCustomAttendeeFields'];
+
+        $content = array();
+        $contentBlocks = get_field('attendee_mail_content_blocks', 'options');
+
+        if (is_array($contentBlocks) && !empty($contentBlocks)) {
+            foreach ($contentBlocks as $block) {
+                if (empty($block['content'])) {
+                    continue;
+                }
+
+                if (!empty($block['rules']) && is_array($block['rules'])) {
+                    $bail = false;
+                    foreach ($block['rules'] as $rule) {
+                        if (
+                            !$bail && $rule['condition'] === 'is-equal' && isset($customAttendeeFields[$rule['custom_attendee_field']]) && $customAttendeeFields[$rule['custom_attendee_field']] == $rule['value']
+                            || !$bail && $rule['condition'] === 'is-not-equal' && !isset($customAttendeeFields[$rule['custom_attendee_field']])
+                            || !$bail && $rule['condition'] === 'is-not-equal' && $customAttendeeFields[$rule['custom_attendee_field']] != $rule['value']
+                        ) {
+                            // Condition pass
+                            continue;
+                        }
+
+                        // Condition fail
+                        $bail = true;
+                    }
+                }
+
+                // Skip
+                if (isset($bail) && $bail) {
+                    continue;
+                }
+
+                $content[] = $block['content'];
+            }
+        }
+
+        return implode('<br>', $content);
+    }
+}
--- wp-content/themes/h22/library/Plugins/Woocommerce/FooEvents.php
+++ wp-content/themes/h22/library/Plugins/Woocommerce/FooEvents.php
@@ -6,117 +6,46 @@ class FooEvents
 {
     public function __construct()
     {
-        add_filter('wp_mail', array($this, 'addCustomFilterForAttendeeMails'), 20, 1);
-        add_filter('H22/Plugins/Woocommerce/FooEvents/attendeeTicketMail', array($this, 'customizeAttendeeMails'), 10, 3);
         add_action('init', array($this, 'registerOptionsPage'));
         add_filter('acf/load_field/key=field_5d6eb5a2a3d77', array($this, 'populateSelectWithAtendeeFields'));
     }
 
-    public static function withHtmlWrapper($content, $title)
+    public function populateSelectWithAtendeeFields($field)
     {
-        $email = wc_get_template_html('emails/email-header.php', array('email_heading' => $title));
-        $email .= '<style>' . wc_get_template_html('emails/email-styles.php') . '</style>';
-        $email .= $content;
-        $email .= wc_get_template_html('emails/email-footer.php');
-        $email = str_replace('{site_title}', 'H22', $email);
-        return $email;
+        $attendeeFields = self::findAllCustomAttendeeFields();
+        $field['choices'] = array_combine($attendeeFields, $attendeeFields);
+        return $field;
     }
 
-    /**
-     * Appends a custom filter hook to simplify customization of attendee ticket mail.
-     *
-     * @param array $args A compacted array of wp_mail() arguments, including the "to" email, subject, message, headers, and attachments values.
-     * @return array
-     */
-    public function addCustomFilterForAttendeeMails($args)
+    public function registerOptionsPage()
     {
-        if (!isset($args['subject'])) {
-            return $args;
+        if (function_exists('acf_add_options_sub_page')) {
+            acf_add_options_sub_page(array(
+                'page_title'     => 'Attendee Email',
+                'menu_title'    => 'Attendee Email',
+                'parent_slug'    => 'woocommerce',
+            ));
         }
-
-        // Get order id from mail subject field
-        preg_match_all('/\[#(\d*?)\] Ticket/m', $args['subject'], $matches, PREG_SET_ORDER, 0);
-        $orderId = isset($matches[0]) && isset($matches[0][1]) ? $matches[0][1] : false;
-
-        if ($orderId && get_post_type($orderId) === 'shop_order') {
-            $order = wc_get_order($orderId);
-            $eventMeta = $order->get_meta('WooCommerceEventsOrderTickets');
-
-            // Make sure event meta exists
-            if (!empty($eventMeta)) {
-                // Get correct attendee meta based on email found in "to".
-                $attendeeMeta = array_values(array_values(array_filter(
-                    $eventMeta,
-                    function ($metaItem) use ($args) {
-                        return isset($metaItem[1])
-                            && isset($metaItem[1]['WooCommerceEventsAttendeeEmail'])
-                            && $metaItem[1]['WooCommerceEventsAttendeeEmail'] === $args['to'];
-                    }
-                ))[0])[0];
-
-                $customAttendeeFields = $attendeeMeta['WooCommerceEventsCustomAttendeeFields'];
-
-                // Manipulate mail here through filter
-                $args = apply_filters('H22/Plugins/Woocommerce/FooEvents/attendeeTicketMail', $args, $customAttendeeFields, $attendeeMeta);
-            }
-        }
-
-        return $args;
     }
 
-    public function customizeAttendeeMails($args, $customAttendeeFields, $attendeeMeta)
+    public static function getAttendees($orderId)
     {
-        $content = array();
-        $contentBlocks = get_field('attendee_mail_content_blocks', 'options');
+        $attendees = array();
 
-        if (is_array($contentBlocks) && !empty($contentBlocks)) {
-            foreach ($contentBlocks as $block) {
-                if (empty($block['content'])) {
-                    continue;
-                }
-
-                if (!empty($block['rules']) && is_array($block['rules'])) {
-                    $bail = false;
-                    foreach ($block['rules'] as $rule) {
-                        if (
-                            !$bail && $rule['condition'] === 'is-equal' && isset($customAttendeeFields[$rule['custom_attendee_field']]) && $customAttendeeFields[$rule['custom_attendee_field']] == $rule['value']
-                            || !$bail && $rule['condition'] === 'is-not-equal' && !isset($customAttendeeFields[$rule['custom_attendee_field']])
-                            || !$bail && $rule['condition'] === 'is-not-equal' && $customAttendeeFields[$rule['custom_attendee_field']] != $rule['value']
-                        ) {
-                            // Condition pass
-                            continue;
-                        }
-
-                        // Condition fail
-                        $bail = true;
-                    }
-                }
-
-                // Skip
-                if (isset($bail) && $bail) {
-                    continue;
-                }
-
-                $content[] = $block['content'];
-            }
+        if (!empty(get_post_meta($orderId, 'WooCommerceEventsOrderTickets', false))) {
+            $attendees = array_values(array_values(array_values(get_post_meta($orderId, 'WooCommerceEventsOrderTickets', false))[0])[0]);
         }
 
-        // Replace subject
-        if (!empty(get_field('attendee_mail_custom_subject', 'options'))) {
-            $args['subject'] = get_field('attendee_mail_custom_subject', 'options');
-        }
+        return $attendees;
+    }
 
-        // Remove attachments
-        if (get_field('attendee_mail_remove_attachments', 'options')) {
-            $args['attachments'] = array();
-        }
+    public static function getAttendeeByMail($orderId, $email)
+    {
+        $attendee = array_filter(self::getAttendees($orderId), function ($attendee) use ($email) {
+            return isset($attendee['WooCommerceEventsAttendeeEmail']) && $attendee['WooCommerceEventsAttendeeEmail'] === $email;
+        });
 
-        // Replace message
-        if (!empty($content)) {
-            $args['message'] = self::withHtmlWrapper(implode('<br>', $content), $args['subject']);
-        }
-
-        return $args;
+        return is_array($attendee) && !empty($attendee) ? array_values($attendee)[0] : false;
     }
 
     public static function findAllCustomAttendeeFields()
@@ -156,23 +85,5 @@ class FooEvents
         }
 
         return $checkBoxFields;
-    }
-
-    public function populateSelectWithAtendeeFields($field)
-    {
-        $attendeeFields = self::findAllCustomAttendeeFields();
-        $field['choices'] = array_combine($attendeeFields, $attendeeFields);
-        return $field;
-    }
-
-    public function registerOptionsPage()
-    {
-        if (function_exists('acf_add_options_sub_page')) {
-            acf_add_options_sub_page(array(
-                'page_title'     => 'Attendee Email',
-                'menu_title'    => 'Attendee Email',
-                'parent_slug'    => 'woocommerce',
-            ));
-        }
     }
 }
