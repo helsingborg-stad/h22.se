bd5203018c80d54532e6772900e4cd804fdbabe4
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-09-10 11:17:47 +0200

    add prefix for object-fit polyfill through post-css

--- wp-content/themes/h22/webpack.config.js
+++ wp-content/themes/h22/webpack.config.js
@@ -78,7 +78,7 @@ module.exports = {
                     {
                         loader: 'postcss-loader',
                         options: {
-                            plugins: [autoprefixer],
+                            plugins: [autoprefixer, require('postcss-object-fit-images')],
                             sourceMap: true,
                         },
                     },
