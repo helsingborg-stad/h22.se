a276e40218d1d5750b27bd3768d564f68d5f3669
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-09-10 08:29:05 +0200

    move smooth scroll into own file

--- wp-content/themes/h22/assets/source/js/app.js
+++ wp-content/themes/h22/assets/source/js/app.js
@@ -4,16 +4,9 @@ import objectFitImages from 'object-fit-images';
 import './menu.js';
 import './video-popup';
 import './vc-tta-section';
+import './smoothScroll';
 
 objectFitVideos();
 objectFitImages();
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
