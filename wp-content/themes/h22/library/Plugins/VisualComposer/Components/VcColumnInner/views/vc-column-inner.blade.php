50c2d722ffdbb20f94e08a70352fac720af51b60
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-10-29 10:42:54 +0100

    Revert "Copy column & row components and register as inner row/col"
    This reverts commit 02a6aa84fcec2437f3abf107321df3e000df5261.

--- wp-content/themes/h22/library/Plugins/VisualComposer/Components/VcColumnInner/views/vc-column-inner.blade.php
+++
@@ -1,5 +0,0 @@
-<div{!! html_build_attributes($attributes) !!}>
-    <div{!! html_build_attributes($childAttributes) !!}>
-        {!! $content !!}
-    </div>
-</div>
--- wp-content/themes/h22/library/Plugins/VisualComposer/Components/VcRowInner/views/vc-row-inner.blade.php
+++
@@ -1,6 +0,0 @@
-<div{!! html_build_attributes($containerAttributes) !!}>
-    <div{!! html_build_attributes($attributes) !!}>
-        {!! $content !!}
-    </div>
-</div>
-{{-- {!! $afterOutput !!} --}}
