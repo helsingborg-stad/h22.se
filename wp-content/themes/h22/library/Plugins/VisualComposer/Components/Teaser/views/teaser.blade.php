52d04ed97a91019fe38a42272c7585de3884fd4f
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-10-29 17:19:43 +0100

    Do some checks

--- wp-content/themes/h22/library/Plugins/VisualComposer/Components/Teaser/views/teaser.blade.php
+++ wp-content/themes/h22/library/Plugins/VisualComposer/Components/Teaser/views/teaser.blade.php
@@ -1,13 +1,17 @@
 <div class="c-element">
     <div{!! html_build_attributes($attributes) !!}>
+        @if (!empty($meta_heading))
         <div class="c-teaser__meta">{{ $meta_heading }}</div>
+        @endif
+        @if (!empty($heading))
         <h2 class="h1">{{ $heading }}</h2>
+        @endif
         @if (!empty($preamble))
             <p class="preamble">{{ $preamble }}</p>
         @endif
        
         {!! $content !!}
-        @if ($link)
+        @if (!empty($link))
             <a{!! html_build_attributes($link['attributes']) !!}>
                 {{ $link['text'] }}
             </a>
