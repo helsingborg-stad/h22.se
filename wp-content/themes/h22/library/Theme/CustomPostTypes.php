0840d70325ffd2ef495ad227a52d278e52239499
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-01-14 15:31:40 +0100

    Add toolbox CPT

--- wp-content/themes/h22/library/Theme/CustomPostTypes.php
+++ wp-content/themes/h22/library/Theme/CustomPostTypes.php
@@ -69,6 +69,37 @@ class CustomPostTypes
         );
 
         new \H22\Entity\PostType(
+            _x('Toolboxes', 'Post type plural', 'h22'),
+            _x('Toolbox', 'Post type singular', 'h22'),
+            'toolbox',
+            array(
+                'description' => __('H22 Projects', 'h22'),
+                'menu_icon' => 'dashicons-portfolio',
+                'public' => true,
+                'publicly_queriable' => true,
+                'show_ui' => true,
+                'show_in_nav_menus' => true,
+                'has_archive' => true,
+                // 'rewrite'              =>   array(
+                //     'slug'       =>   __('projects', 'h22'),
+                //     'with_front' =>   false
+                // ),
+                'hierarchical' => false,
+                'exclude_from_search' => false,
+                'taxonomies' => array('category', 'post_tag'),
+                'supports' => array(
+                    'title',
+                    'editor',
+                    'author',
+                    'thumbnail',
+                    'excerpt',
+                ),
+                'show_in_rest' => true,
+                'menu_position' => 10,
+            )
+        );
+
+        new \H22\Entity\PostType(
             _x('Speakers', 'Post type plural', 'h22'),
             _x('Speaker', 'Post type singular', 'h22'),
             'speaker',
