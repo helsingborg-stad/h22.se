474b45c17dd8b81e86317ea955a91f6ccdbae837
nRamstedt <nikolas.ramstedt@gmail.com>
2020-03-04 15:56:05 +0100

    Register posttype Partners

--- wp-content/themes/h22/library/Theme/CustomPostTypes.php
+++ wp-content/themes/h22/library/Theme/CustomPostTypes.php
@@ -129,5 +129,36 @@ class CustomPostTypes
                 'menu_position' => 10,
             )
         );
+
+        new \H22\Entity\PostType(
+            _x('Partners', 'Post type plural', 'h22'),
+            _x('Partner', 'Post type singular', 'h22'),
+            'partners',
+            array(
+                'description' => __('H22 Partners', 'h22'),
+                'menu_icon' => 'dashicons-portfolio',
+                'public' => true,
+                'publicly_queriable' => true,
+                'show_ui' => true,
+                'show_in_nav_menus' => true,
+                'has_archive' => true,
+                // 'rewrite'              =>   array(
+                //     'slug'       =>   __('projects', 'h22'),
+                //     'with_front' =>   false
+                // ),
+                'hierarchical' => false,
+                'exclude_from_search' => false,
+                'taxonomies' => array('category', 'post_tag'),
+                'supports' => array(
+                    'title',
+                    'editor',
+                    'author',
+                    'thumbnail',
+                    'excerpt',
+                ),
+                'show_in_rest' => true,
+                'menu_position' => 10,
+            )
+        );
     }
 }
