e98f7f469d92eae957139133a71952ec03a4828b
nRamstedt <nikolas.ramstedt@gmail.com>
2020-05-29 09:24:01 +0200

    add partner cards

---
+++ wp-content/themes/h22/bem-views/partials/archive/post/post-partner.blade.php
@@ -0,0 +1,44 @@
+<?php global $post;
+
+$image = get_the_post_thumbnail_url(get_queried_object_id(), 'full');
+$terms = get_the_terms(get_queried_object_id(), get_post_type() === 'project' ? 'organisation' : 'post_tag');
+
+if (is_array($terms) && !empty($terms)) {
+    $terms = implode(array_map(function($termObject) {
+        return $termObject->name;
+    }, $terms), ', ');
+}
+?>
+
+<div class="c-card c-card--clickable @empty($image) c-card--without-image @endempty">
+    <div class="c-card__image-wrapper c-card__image-wrapper--4by3">
+        @if( $image )
+            <img class="c-card__image c-card__image--contain u-p-4" src="{{ $image }}"/>
+        @endif
+    </div>
+
+    <div class="c-card__body">
+        @if(get_field('archive_' . sanitize_title(get_post_type()) . '_feed_date_published', 'option') )
+            <div class="c-card__meta">
+                @if ($terms)
+                    {{$terms}}
+                @else 
+                    {{get_post_type_object(get_post_type())->labels->singular_name}}
+                @endif
+            </div>
+        @endif
+        <h2 class="c-card__title">
+            <a href="{{ the_permalink() }}" class="c-card__link">
+                {{ the_title() }}
+            </a>
+        </h2>
+        {{-- <div class="c-card__text">
+            {{\H22\Content\Excerpt::getExcerpt($post->ID, 24, true)}}
+        </div> --}}
+    </div>
+    {{-- <div class="c-card__footer">
+            <a class="c-card__link c-card__link--read-more c-card__link--right" href="{{get_permalink()}}">
+                Read more
+            </a>
+        </div> --}}
+</div>
--- wp-content/themes/h22/library/Theme/Archive.php
+++ wp-content/themes/h22/library/Theme/Archive.php
@@ -8,14 +8,15 @@ class Archive
         add_filter('acf/load_field/name=archive_projects_post_style', array($this, 'customPostStyleTemplates'), 10, 3);
         add_filter('acf/load_field/name=archive_toolbox_post_style', array($this, 'customPostStyleTemplates'), 10, 3);
         add_filter('acf/load_field/name=archive_news_post_style', array($this, 'customPostStyleTemplates'), 10, 3);
-        add_filter('acf/load_field/name=archive_post_post_style', array($this, 'customPostStyleTemplates'), 10, 3);        
+        add_filter('acf/load_field/name=archive_post_post_style', array($this, 'customPostStyleTemplates'), 10, 3);
+        add_filter('acf/load_field/name=archive_partners_post_style', array($this, 'customPostStyleTemplates'), 10, 3);
     }
 
     public function customPostStyleTemplates($field)
     {
         $field['choices']['projects'] = 'Project Cards (H22 Theme)';
         $field['choices']['news'] = 'News Cards (H22 Theme)';
-
+        $field['choices']['partner'] = 'Partner Cards (H22 Theme)';
 
         return $field;
     }
